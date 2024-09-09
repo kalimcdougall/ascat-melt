1a4f2c3cdb27b224d6119a605a72a962fa1610a3
Kali McDougall <152206591+kalimcdougall@users.noreply.github.com>
2024-09-09 16:25:20 -0700

    Update melt_detection.py

--- melt_detection.py
+++ melt_detection.py
@@ -8,8 +8,8 @@ from datetime import datetime, timedelta
 
 dir = 'D:/Drive Backup/School/Masters/Data/ASCAT/'
 
-clip_melt_ds = nc.Dataset(dir + 'clipped_melt.nc')
-clip_sigma0 = clip_melt_ds.variables['clip_sigma0']
+ds = nc.Dataset(dir + 'clip_sigma0.nc')
+clip_sigma0 = ds.variables['clip_sigma0']
 
 # Calculate the previous winter (Jun 1 - Aug 31) mean - for melt binary variable
 winter_mean = np.mean(clip_sigma0[:91], axis=0) # Define index range of winter dates
