1a8beddae921c41f8a8721bffaa7e031d62d0711
Kali McDougall <152206591+kalimcdougall@users.noreply.github.com>
2024-09-10 10:48:46 -0700

    Update melt_detection.py

--- melt_detection.py
+++ melt_detection.py
@@ -9,7 +9,7 @@ clip_sigma0 = ds.variables['Sigma0']
 # Calculate the previous winter (Jun 1 - Aug 31) mean - for melt binary variable
 winter_mean = np.mean(clip_sigma0[:91], axis=0) # Define index range of winter dates
 
-#%%
+#======================================================
 # Apply NaN mask to melt_binary values before writing to netCDF variable
 # Otherwise, NaN values will be assigned a fill value of -2147483648 which must be removed before plotting
 
@@ -19,7 +19,7 @@ data['melt_binary'] = (('time', 'y', 'x'), melt_binary)
 data['melt_binary'].attrs['long_name'] = 'Melt binary'
 data['melt_binary'].attrs['units'] = 'boolean'
 
-#%%
+#======================================================
 # Export file as netCDF
 melt = data.melt_binary
 output_file = dir + 'melt_binary.nc'
